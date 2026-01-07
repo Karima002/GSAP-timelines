e471f34827d9d1e8344cdd67b8f8e5585742e113
Karima002 <karima.mouadi@hva.nl>
2026-01-07 20:44:29 +0100

    #1 function toegevoegd

--- static/js/scene-four.js
+++ static/js/scene-four.js
@@ -4,7 +4,7 @@ import { DOM } from './elements.js'
 export function createSceneFour() {
   return gsap.timeline({ defaults: {duration: 1 }, paused: true })
     .pause()
-    .add(tl(), .25)
+    .add(tlMeteoriet(), .25)
 }
 
 /* child timelines */
@@ -14,3 +14,9 @@ function tl() {
     .to(document.body, .5, { rotation: 0, x: 0, scale:1, skewX:0, ease: 'elastic.out(1, 0.3)' }, '+=0.25')
 }
 
+
+function tlMeteoriet() {
+  return gsap.timeline({})
+    .set(DOM.meteoriet, {xPercent:100, x:'120%', scale: 0.15, opacity:1, transformOrigin:'50% 0'}) //voor scene 4. svg heeft een andere assenstelsel dan HTML. transformOrgin geeft aan vanaf welk punt je animatie begint. Roteren vanuit het middelpunt is 50% 50%. Maar je ook linksboven roteren. TransformOrgin bepaalt het punt waar je om heen transformeert.
+    .to(DOM.meteoriet, 1,  {opacity:1}, '+=.25')
+}
